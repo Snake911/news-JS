d4ca10a5441449db38b20803ad9d29a23de0fc46
Volkov Danil <snake1234567890-1@yandex.ru>
2022-06-18 12:28:37 +0300

    refactor: add module typescript, configure webpack and eslint

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '1b5795b8e6754b4cba7cf2e2a94b1833',
         });
     }
 }
